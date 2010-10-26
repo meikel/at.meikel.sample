4cb8b9baa29b99a469b05b2f5bce8c3d198a7c1d
Meikel <meikel@meikel.at>
2010-10-26 08:04:50 +0200

    Modified test case for PrimeNumberCalculator

--- at.meikel.sample.aspectj/src/test/java/logic/PrimeNumberCalculatorTest.java
+++ at.meikel.sample.aspectj/src/test/java/logic/PrimeNumberCalculatorTest.java
@@ -1,5 +1,7 @@
 package logic;
 
+import java.util.Arrays;
+
 import junit.framework.TestCase;
 
 public class PrimeNumberCalculatorTest extends TestCase {
@@ -29,15 +31,11 @@ public class PrimeNumberCalculatorTest extends TestCase {
 	}
 
 	public void testPrimeNumbersBetween10And1() {
-		int[] numbers = PrimeNumberCalculator.primes(10, 1);
-		assertNotNull("Result for primes between 10 and 1 is not null.",
-				numbers);
-		assertEquals("4 prime numbers exist between 10 and 1", 4,
-				numbers.length);
-		assertEquals("1st prime number between 10 and 1 is 2.", 2, numbers[0]);
-		assertEquals("2nd prime number between 10 and 1 is 3.", 3, numbers[1]);
-		assertEquals("3rd prime number between 10 and 1 is 5.", 5, numbers[2]);
-		assertEquals("4th prime number between 10 and 1 is 7.", 7, numbers[3]);
+		int[] expected = PrimeNumberCalculator.primes(1, 10);
+		int[] actual = PrimeNumberCalculator.primes(10, 1);
+		assertTrue(
+				"Result for primes between 10 and 1 equals result of primes between 1 and 10.",
+				Arrays.equals(expected, actual));
 	}
 
 }
